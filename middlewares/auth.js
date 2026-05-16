73d70ed72d71d02c22ae528cbe8664e65372c7b4
shimaa <shimaamohiy16@gmail.com>
2026-05-16 06:47:51 +0300

    feat: add JWT auth middleware and hashing utils

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,9 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 exports.verifyToken = (req, res, next) => {
-  let token = req.cookies.Authorization?.split(" ")[1];
+  let token = req.cookies.Authorization?.split(' ')[1];
   if (!token && req.headers.authorization) {
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(' ')[1];
   }
 
   if (!token) return res.status(401).json({ message: 'Unauthorized' });
@@ -17,10 +17,10 @@ exports.verifyToken = (req, res, next) => {
   }
 };
 exports.checkRole = (roles) => {
-    return (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
-        return res.status(403).json({ message: 'Unauthorized access for this role' });
-      }
-      next();
-    };
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Unauthorized access for this role' });
+    }
+    next();
   };
+};
--- utils/hashing.js
+++ utils/hashing.js
@@ -1,11 +1,11 @@
-const { hash, compare } = require("bcryptjs");
+const { hash, compare } = require('bcryptjs');
 
-exports.doHash = (value, saltValue)=>{
-    const result = hash(value, saltValue);
-    return result;
+exports.doHash = (value, saltValue) => {
+  const result = hash(value, saltValue);
+  return result;
 };
 
-exports.doHashValidation = (value, hashedValue)=>{
-    const result = compare(value, hashedValue);
-    return result;
+exports.doHashValidation = (value, hashedValue) => {
+  const result = compare(value, hashedValue);
+  return result;
 };
